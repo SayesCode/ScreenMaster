8bec9e214348b4de20eeba2124d4c46ccfb72c30
I'm Matheus <codelong@proton.me>
2024-10-20 13:02:49 -0300

    Web

--- README.md
+++
@@ -1,2 +0,0 @@
-# ScreenMaster
-ScreenMaster by Sayes
--- screenmaster/android/app/src/main/kotlin/com/example/screenmaster/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.screenmaster
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity()
